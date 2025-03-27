78e2982938a78b4a0d763183091624ed418ea597
Emre-Guzel-2 <yunus.emre.guzel@mths.ca>
2025-03-27 11:17:43 -0400

    finshed the project

--- js/script.js
+++ js/script.js
@@ -1,16 +1,17 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
 // Created by: Emre Guzel
-// Created on: March 20  2025
+// Created on: March 27  2025
 // This file contains the JS functions for index.html
 
 "use strict"
 
-function calculatoinOfAreaOftriangle() {
+function calculatoinOfAreaOfTrapizoid() {
   // Setting the varibles
-  const baseOfTheTriangle = parseFloat(document.getElementById("base-of-the-triangle").value)
-  const heightOfTheTriangle = parseFloat (document.getElementById("height-of-the-triangle").value)
-  const calculatoinOfAreaOftriangle = (baseOfTheTriangle * heightOfTheTriangle) / 2 
+  const baseAOfTrapizoid = parseFloat(document.getElementById("base-a-of-trapizoid").value)
+  const baseBOfTrapizoid = parseFloat (document.getElementById("base-b-of-trapizoid").value)
+  const heightOfTheTrapizoid = parseFloat (document.getElementById("height-of-the-trapizoid").value)
+  const calculatoinOfAreaOfTrapizoid = [(baseAOfTrapizoid + baseBOfTrapizoid) / 2] * heightOfTheTrapizoid
   // clacualtoins 
-  document.getElementById("area").innerHTML ="Area is: " + calculatoinOfAreaOftriangle + "cm²"
+  document.getElementById("area").innerHTML ="Area is: " + calculatoinOfAreaOfTrapizoid + "mm²"
 }
