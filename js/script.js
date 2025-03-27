8c0a3afdbb8f8339356734216dedb8248d5dfee9
Emre-Guzel-2 <yunus.emre.guzel@mths.ca>
2025-03-27 13:21:01 +0000

    added the templete

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Emre Guzel
+// Created on: March 20  2025
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function calculatoinOfAreaOftriangle() {
+  // Setting the varibles
+  const baseOfTheTriangle = parseFloat(document.getElementById("base-of-the-triangle").value)
+  const heightOfTheTriangle = parseFloat (document.getElementById("height-of-the-triangle").value)
+  const calculatoinOfAreaOftriangle = (baseOfTheTriangle * heightOfTheTriangle) / 2 
+  // clacualtoins 
+  document.getElementById("area").innerHTML ="Area is: " + calculatoinOfAreaOftriangle + "cm²"
+}
